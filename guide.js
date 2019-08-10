b9d15d0fe98718f32cd5aab360695b0975380b32
dreadedharvester <tarunkhare54321@gmail.com>
2019-08-10 11:33:35 +0530

    added model file User.js

--- guide.js
+++ guide.js
@@ -91,3 +91,9 @@ app.use('/api/users', require('./routes/api/users'));
 app.use('/api/auth', require('./routes/api/auth'));
 app.use('/api/profile', require('./routes/api/profile'));
 app.use('/api/posts', require('./routes/api/posts'));
+
+/*
+Now to define schema of database, create folder: models in root directory,
+then inside it create User.js to create schema for users.
+In User.js, type:
+*/
