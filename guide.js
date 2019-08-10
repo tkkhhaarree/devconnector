186df6791ac2d1521829ed2f3c0f4d266a50f4c2
dreadedharvester <tarunkhare54321@gmail.com>
2019-08-10 11:25:00 +0530

    added files

--- guide.js
+++ guide.js
@@ -24,4 +24,70 @@ Now in package.json, remove "test" key from "scripts" and instead add:
 "start": "node server",
 "server": "nodemon server"
 (here server is the name of file server.js)
+
+Now save all, type command: npm run server, then open localhost:5000
+
+also to add to git, type: 
+git add .
+git commit -m "initial commit"
+
+now copy connection string from mongodb atlas. create new folder: config, add new file in it: default.json.
+add to this file: 
+{
+    "mongoURI": "your connection string"
+}
+
+add another file in config folder: db.js.
+add code:
 */
+const mongoose = require('mongoose');
+const config = require('config');
+
+const db = config.get('mongoURI'); // fetches variable defined in default.json
+
+const connectDB = async () => {
+  try {
+    await mongoose.connect(db, { useNewUrlParser: true });
+    console.log('MongoDB connected...');
+  } catch (err) {
+    console.error(err.message);
+    process.exit(1);
+  }
+};
+
+module.exports = connectDB;
+
+/*
+  now to call connectDB from server.js, goto server.js and add to it:
+  */
+const connectDB = require('./config/db');
+//connect database:
+connectDB();
+
+/*
+now try: npm run server
+
+now create folder: routes, inside it create folder: api,
+then inside api folder, create files: users.js, auth.js, profile.js, posts.js
+now in users.js type:
+*/
+const express = require('express');
+const router = express.Router();
+
+//@route GET api/users
+//@desc test route
+//@access Public
+router.get('/', (req, res) => res.send('Users route'));
+
+module.exports = router;
+
+/*
+similarly, do this in other files of this folder too (auth.js, profile.js, posts.js).
+
+now in server.js, define routes in the following way:
+*/
+//define routes
+app.use('/api/users', require('./routes/api/users'));
+app.use('/api/auth', require('./routes/api/auth'));
+app.use('/api/profile', require('./routes/api/profile'));
+app.use('/api/posts', require('./routes/api/posts'));
